8c8ca99a64930e2c698eadc90ffb06a1887868e4
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-12 14:41:03 -0400

    Exclude certain folders. Fixes #1

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ var fs = require('fs'),
 // all files in path and path's subfolders
 var getUniqueExtensions = module.exports = function (filepath) {
   // Hash to avoid duplicates and membership lookups
-  var extensions = arguments[1] || {};
+  var extensions = arguments[1] || {},
+      exclusions = ['node_modules', '.git', '.sass-cache'];
 
   // For all file names in the current directory
   fs.readdirSync(filepath).forEach(function (filename) {
 
     var fullName = filepath + '/' + filename;
 
-    // If it's a directory
-    if (fs.lstatSync(fullName).isDirectory()) {
+    // If it's a directory and isn't supposed to be ignored
+    if (fs.lstatSync(fullName).isDirectory() && exclusions.indexOf(filename) === -1) {
       getUniqueExtensions(fullName, extensions);
     }
     else {
