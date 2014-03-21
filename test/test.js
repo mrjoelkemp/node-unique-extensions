a745dc8b3c9a144b61e38fe5f4b3f57efc90ea58
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-21 13:25:49 -0400

    Test example ignored dirs

--- test/test.js
+++ test/test.js
@@ -1,16 +1,14 @@
 var getExtensions = require('../index');
 
 // List of directories to ignore
-var exclusions = ['node_modules', '.git', '.sass-cache', 'bower_components'];
+var exclusions = ['node_modules', '.git', '.sass-cache', 'bower_components', 'vendor'];
 
 // Run from the root folder
-var extensions = getExtensions('.', exclusions);
+var extensions = getExtensions(__dirname, exclusions);
 
 console.log('Extensions found: ', extensions);
-console.log(extensions.length === 6);
+console.log(extensions.length === 4);
 console.log(!! ~extensions.indexOf('.html'));
 console.log(!! ~extensions.indexOf('.css'));
 console.log(!! ~extensions.indexOf('.coffee'));
 console.log(!! ~extensions.indexOf('.js'));
-console.log(!! ~extensions.indexOf('.json'));
-console.log(!! ~extensions.indexOf('.md'));
