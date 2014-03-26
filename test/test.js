101889f3d49c01cc1787da00878a87ef2bbc62ac
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-26 09:07:09 -0400

    Test for dotfiles

--- test/test.js
+++ test/test.js
@@ -4,11 +4,16 @@ var getExtensions = require('../index');
 var exclusions = ['node_modules', '.git', '.sass-cache', 'bower_components', 'vendor'];
 
 // Run from the root folder
-var extensions = getExtensions(__dirname, exclusions);
+var extensions = getExtensions({
+  path: __dirname,
+  exclusions: exclusions,
+  includeDotFiles: true
+});
 
 console.log('Extensions found: ', extensions);
-console.log(extensions.length === 4);
+console.log(extensions.length === 5);
 console.log(!! ~extensions.indexOf('.html'));
 console.log(!! ~extensions.indexOf('.css'));
 console.log(!! ~extensions.indexOf('.coffee'));
 console.log(!! ~extensions.indexOf('.js'));
+console.log(!! ~extensions.indexOf('.jshintrc'));
